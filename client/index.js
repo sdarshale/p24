e991198019c1627ebfc64ff689f29723dcde6e7f
andy <xxxandyfenxxx@xxx.com>
2021-03-17 15:35:59 -0700

    check if isEligible

--- client/index.js
+++ client/index.js
@@ -4,8 +4,8 @@ const order = {
   purchase_units: [
     {
       amount: {
-        currency_code: 'EUR',
-        value: '99.99',
+        currency_code: "EUR",
+        value: "99.99",
       },
     },
   ],
@@ -13,14 +13,14 @@ const order = {
     return_url: `${window.location.origin}/success.html`,
     cancel_url: `${window.location.origin}/cancel.html`,
   },
-}
+};
 
 /* Paypal */
 paypal
   .Marks({
     fundingSource: paypal.FUNDING.PAYPAL,
   })
-  .render('#paypal-mark')
+  .render("#paypal-mark");
 
 paypal
   .Buttons({
@@ -30,115 +30,125 @@ paypal
       color: "silver",
     },
     createOrder(data, actions) {
-      return actions.order.create(order)
+      return actions.order.create(order);
     },
     onApprove(data, actions) {
-      return actions.order.capture().then(function(details) {
-        alert(`Transaction completed by ${details.payer.name.given_name}!`)
-      })
+      return actions.order.capture().then(function (details) {
+        alert(`Transaction completed by ${details.payer.name.given_name}!`);
+      });
     },
   })
-  .render('#paypal-btn')
+  .render("#paypal-btn");
 
 /* P24 */
 paypal
   .Marks({
     fundingSource: paypal.FUNDING.P24,
   })
-  .render('#p24-mark')
+  .render("#p24-mark");
 
 paypal
   .Fields({
     fundingSource: paypal.FUNDING.P24,
     style: {
       base: {
-        backgroundColor: 'white',
-        color: 'black',
-        fontSize: '16px',
+        backgroundColor: "white",
+        color: "black",
+        fontSize: "16px",
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        lineHeight: '1.4',
-        letterSpacing: '0.3',
+        lineHeight: "1.4",
+        letterSpacing: "0.3",
       },
       input: {
-        backgroundColor: 'white',
-        fontSize: '16px',
-        color: '#333',
-        borderColor: '#dbdbdb',
-        borderRadius: '4px',
-        borderWidth: '1px',
-        padding: '1rem',
+        backgroundColor: "white",
+        fontSize: "16px",
+        color: "#333",
+        borderColor: "#dbdbdb",
+        borderRadius: "4px",
+        borderWidth: "1px",
+        padding: "1rem",
       },
       invalid: {
-        color: 'red',
+        color: "red",
       },
       active: {
-        color: 'black',
+        color: "black",
       },
     },
     fields: {
       name: {
-        value: ''
+        value: "",
       },
     },
   })
-  .render('#p24-container')
+  .render("#p24-container");
 
-paypal
-  .Buttons({
-    fundingSource: paypal.FUNDING.P24,
-    upgradeLSAT: true,
-    style: {
-      label: 'pay',
-    },
-    createOrder(data, actions) {
-      return actions.order.create(order)
-    },
-    onApprove(data, actions) {
-      fetch(`/capture/${data.orderID}`, {
-        method: "post",
+const p24Buton = paypal.Buttons({
+  fundingSource: paypal.FUNDING.P24,
+  upgradeLSAT: true,
+  style: {
+    label: "pay",
+  },
+  createOrder(data, actions) {
+    return actions.order.create(order);
+  },
+  onApprove(data, actions) {
+    fetch(`/capture/${data.orderID}`, {
+      method: "post",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        swal(
+          "Order Captured!",
+          `Id: ${data.id}, ${Object.keys(data.payment_source)[0]}, ${
+            data.purchase_units[0].payments.captures[0].amount.currency_code
+          } ${data.purchase_units[0].payments.captures[0].amount.value}`,
+          "success"
+        );
       })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data)
-          swal("Order Captured!", `Id: ${data.id}, ${Object.keys(data.payment_source)[0]}, ${data.purchase_units[0].payments.captures[0].amount.currency_code} ${data.purchase_units[0].payments.captures[0].amount.value}`, "success");
-        })
-        .catch(console.error);
-    },
-    onCancel(data, actions) {
-      console.log(data)
-      swal("Order Canceled", `ID: ${data.orderID}`, "warning");
-    },
-    onError(err) {
-      console.error(err);
-    },
-  })
-  .render('#p24-btn')
+      .catch(console.error);
+  },
+  onCancel(data, actions) {
+    console.log(data);
+    swal("Order Canceled", `ID: ${data.orderID}`, "warning");
+  },
+  onError(err) {
+    console.error(err);
+  },
+});
+
+if (p24Buton.isEligible()) {
+  p24Buton.render("#p24-btn");
+} else {
+  document.getElementById("p24-radio").style.display = "none";
+}
 
 /* Radio buttons */
 // Listen for changes to the radio buttons
-document.querySelectorAll('input[name=payment-option]').forEach(el => {
+document.querySelectorAll("input[name=payment-option]").forEach((el) => {
   // handle button toggles
-  el.addEventListener('change', event => {
+  el.addEventListener("change", (event) => {
     switch (event.target.value) {
-      case 'paypal':
-        document.getElementById('p24-container').style.display = 'none'
-        document.getElementById('p24-btn').style.display = 'none'
+      case "paypal":
+        document.getElementById("p24-container").style.display = "none";
+        document.getElementById("p24-btn").style.display = "none";
 
-        document.getElementById('paypal-btn').style.display = 'block'
+        document.getElementById("paypal-btn").style.display = "block";
 
-        break
-      case 'p24':
-        document.getElementById('p24-container').style.display = 'block'
-        document.getElementById('p24-btn').style.display = 'block'
+        break;
+      case "p24":
+        document.getElementById("p24-container").style.display = "block";
+        document.getElementById("p24-btn").style.display = "block";
 
-        document.getElementById('paypal-btn').style.display = 'none'
-        break
+        document.getElementById("paypal-btn").style.display = "none";
+        break;
 
       default:
-        break
+        break;
     }
-  })
-})
+  });
+});
 
-document.getElementById('p24-container').style.display = 'none'
-document.getElementById('p24-btn').style.display = 'none'
+document.getElementById("p24-container").style.display = "none";
+document.getElementById("p24-btn").style.display = "none";
